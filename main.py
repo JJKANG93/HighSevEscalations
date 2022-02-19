c462a68a70e716fa3d804bb8882de7f08283521b
MrChrisLia <60084418+MrChrisLia@users.noreply.github.com>
2022-02-19 22:37:28 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,26 @@
+escalation_Template = {
+  "Status:": "",
+  "Severity:": "",
+  "Name:": "",
+  "Affecting System:": "",
+  "Tier:":" T",
+  "Operator:": "",
+  "Time Elapsed:": "",
+  "Start Time:": "",
+  "End Time:": "",
+  "Service Degradation:": "",
+  "Symptoms:": "",
+  "Action Taken:": "",
+  "Root Cause:": "",
+  "Comms Manager:": "",
+  "Crisis Manager:": "",
+  "Escalated by:": "",
+
+  "Clik ID:": "",
+  "Customer Ref#:": "",
+
+  "Join Microsoft Teams Chat:": "",
+  }
+
+for key in escalation_Template:
+  print(key + "\n")
